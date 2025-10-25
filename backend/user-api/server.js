b627aa587fe6564e013c262ff3bb895549e7a189
Jake M <jakemanson77@icloud.com>
2025-10-25 19:13:09 +0100

    chore: setup Prisma, dotenv, and update docker-compose/frontend paths

---
+++ backend/user-api/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+
+app.use(cors ({
+    origin: ['http://localhost:5173', 'http://localhost:5174']
+
+}))
+
+const prisma = new PrismaClient();
+
+
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
